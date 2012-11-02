4b2e68c1c30938a2d12255591fef27850a47719f
John Francis Mukulu <john.f.mukulu@gmail.com>
2012-11-02 12:32:12 +0300

    applying # in IDs

--- deathregistry.js
+++ deathregistry.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-	var wasDateInvestigated='83398-238613-val';
-	var investigationDate='83398-238616-val';
+	var wasDateInvestigated='#83398-238613-val';
+	var investigationDate='#83398-238616-val';
+	$(investigationDate).attr('disabled', 'true');
 	$(wasDateInvestigated).on('change', function() {
 		if( ($(wasDateInvestigated).val()=='true'))  {
 			$(investigationDate).removeAttr('disabled');
